39b6d704c3267bff147ecf5230f27296966530c5
Datum Geek <mike.graham@ihsmarkit.com>
2016-10-26 04:54:04 -0600

    fix compiler issues

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,19 @@
 //
 // Licensed under MIT open source license http://opensource.org/licenses/MIT
 //
-export {Pak} from './pak/pak';
+export {Pak, PakJSON} from './pak/pak';
+export {PakDirectory, PakDirectoryJSON} from './pak/pakDirectory';
+export {PakRepository, PakRepositoryJSON, PakRepositoryType} from './pak/pakRepository';
+export {View, ViewJSON} from './pak/view';
+
+export {ActivePak, ActivePakJSON} from './state/activePak';
+export {StateDirectory, StateDirectoryJSON} from './state/stateDirectory';
+export {StateRepository, StateRepositoryJSON, StateRepositoryType} from './state/stateRepository';
+export {StateSession, StateSessionJSON} from './state/stateSession';
+export {ViewInstance, ViewInstanceJSON} from './state/viewInstance';
+
+export {PlotterShellModel} from './plotterShellModel';
+export {IFileManager} from './util';
 
 import * as _util from './util';
 export var util = _util;
---
+++ src/lib/plotterShellModel.ts
@@ -0,0 +1,108 @@
+import { StateDirectory } from './state/stateDirectory';
+import { StateRepository } from './state/stateRepository';
+import { StateSession } from './state/stateSession';
+import { IFileManager } from './util';
+import { ElectronHelper } from './electronHelper';
+import { PhoneGapHelper } from './phoneGapHelper';
+
+export class PlotterShellModel {
+
+    public electronHelper = new ElectronHelper();
+    public phoneGapHelper = new PhoneGapHelper();
+
+    constructor(public fileManager: IFileManager) {}
+
+    public start(): Promise<StateDirectory> {
+        let that = this;
+
+        return new Promise<StateDirectory>((resolve, reject) => {
+
+            let sdn = that.stateDirectoryName;
+
+            // check if sdn has prefix (service:, githubgist:myStateDir[.json], localstorage:)
+            if (sdn.toLowerCase().startsWith('service:')) {
+                reject('service not supported yet.');
+            } else
+                if (sdn.toLowerCase().startsWith('githubgist:')) {
+                    reject('githubgist not supported yet.');
+                } else
+                    if (sdn.toLowerCase().startsWith('localstorage:')) {
+                        reject('localstorage not supported yet.');
+                    } else {
+
+                        // check if (and use) platform origin has state-directory
+                        // this.httpClient.baseUrl = 'http://localhost:9000/';
+
+                        if (that.electronHelper.isElectron) {
+                            let fs = that.electronHelper.fs;
+                            let resourcePath = that.electronHelper.userDataPath;
+
+                            fs.readFile(`${resourcePath}/${sdn}.json`, (err: any, stringData: any) => {
+                                if (err) {
+                                    reject(err);
+                                    return;
+                                }
+
+                                let data = JSON.parse(stringData);
+
+                                let stateDirectory = StateDirectory.fromJSON(that.fileManager, data);
+                                that.stateDirectory = stateDirectory;
+                                resolve(stateDirectory);
+                                return;
+                            });
+                        } else if (that.phoneGapHelper.isPhoneGap) {
+                            that.phoneGapHelper.readFromFile(`${sdn}.json`)
+                                .then((o: any) => {
+                                    let stateDirectory = StateDirectory.fromJSON(that.fileManager, o);
+                                    that.stateDirectory = stateDirectory;
+                                    resolve(stateDirectory);
+                                })
+                                .catch(r => reject(r));
+                        } else {
+                            that.fileManager.get([`${sdn}.json`])
+                                .then(response => {
+                                    return JSON.parse(response);
+                                })
+                                .then(data => {
+                                    let stateDirectory = StateDirectory.fromJSON(that.fileManager, data);
+                                    that.stateDirectory = stateDirectory;
+                                    resolve(stateDirectory);
+                                })
+                                .catch(reason => {
+                                    reject(new Error(`fetch state-dictionary2: reason: \r\n\r\n${reason}`));
+                                });
+
+                            // check if (and use) local storage has state-directory
+
+                            // create state-directory in local storage
+                        }
+                    }
+        });
+    }
+
+    public stateDirectoryName: string = 'state-directory';
+
+    private myStateDirectory: StateDirectory;
+    public get stateDirectory() {
+        return this.myStateDirectory;
+    }
+    public set stateDirectory(value) {
+        this.myStateDirectory = value;
+    }
+
+    private myStateRepository: StateRepository;
+    public get stateRepository() {
+        return this.myStateRepository;
+    }
+    public set stateRepository(value) {
+        this.myStateRepository = value;
+    }
+
+    private myStateSession: StateSession;
+    public get stateSession() {
+        return this.myStateSession;
+    }
+    public set stateSession(value) {
+        this.myStateSession = value;
+    }
+}
